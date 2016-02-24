25f9cb4a18097c5619f8b77401971f86615c9f01
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-02-24 19:26:32 +0530

    executable programs to demo variable

---
+++ golang_workshop/code/variable1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// START OMIT
+	var age int
+	var tall bool
+	var name, place string
+	fmt.Println(age, tall, name, place)
+	// END OMIT
+}
---
+++ golang_workshop/code/variable2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// START OMIT
+	var age int = 10
+	var tall = true
+	var name, place string = "Jack", "USA"
+	fmt.Println(age, tall, name, place)
+	// END OMIT
+}

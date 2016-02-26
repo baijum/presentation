796c59c7e6b842ffdfb4eb8a4fa5656463902460
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-02-26 10:02:27 +0530

    pipeline code from golang blog

---
+++ golang_workshop/code/pipeline1.go
@@ -0,0 +1,36 @@
+// Code from https://blog.golang.org/pipelines
+package main
+
+import "fmt"
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	// Set up the pipeline.
+	c := gen(2, 3)
+	out := sq(c)
+
+	// Consume the output.
+	fmt.Println(<-out) // 4
+	fmt.Println(<-out) // 9
+}
---
+++ golang_workshop/code/pipeline2.go
@@ -0,0 +1,33 @@
+// Code from https://blog.golang.org/pipelines
+package main
+
+import "fmt"
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	// Set up the pipeline and consume the output.
+	for n := range sq(sq(gen(2, 3))) {
+		fmt.Println(n) // 16 then 81
+	}
+}

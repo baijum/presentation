8fc3101aafe1159309ca1dea3f72b961cbd49142
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-04-01 09:52:27 +0530

    highlight and reorganize slides

--- gorilla_websocket/example1.go
+++ gorilla_websocket/example1.go
@@ -1,11 +1,14 @@
 package main
 
+// START IMPORT OMIT
 import (
 	"net/http"
 	"time"
 
-	"github.com/gorilla/websocket"
+	"github.com/gorilla/websocket" // HL
 )
+
+// END IMPORT OMIT
 
 // START UPGRADER OMIT
 var upgrader = websocket.Upgrader{
@@ -20,21 +23,25 @@ func writer(ws *websocket.Conn) {
 	defer ws.Close()
 
 	for {
-		ws.WriteMessage(websocket.TextMessage, []byte("hello"))
+		ws.WriteMessage(websocket.TextMessage, []byte("hello")) // HL
 		time.Sleep(2 * time.Second)
 	}
 }
 
 // END WRITER OMIT
+
 // START SERVE OMIT
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, _ := upgrader.Upgrade(w, r, nil)
+	ws, _ := upgrader.Upgrade(w, r, nil) // HL
 	go writer(ws)
 }
 
+// END SERVE OMIT
+
+// START MAIN OMIT
 func main() {
-	http.HandleFunc("/ws", serveWs)
+	http.HandleFunc("/ws", serveWs) // HL
 	http.ListenAndServe(":8081", nil)
 }
 
-// END SERVE OMIT
+// END MAIN OMIT
